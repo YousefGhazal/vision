d4b86c74f549efcae66132dd74161b3e16a76717
Yousef <yousefghazal2412@gmail.com>
2023-04-04 06:41:54 +0200

    make it python 3

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
